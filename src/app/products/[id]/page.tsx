87905a080c2fee383dbd9571f938bef0e17a67dc
unknown <mustafamjaber@yahoo.com>
2023-10-29 11:57:02 +0200

    add the carousel image for the singel product page

--- src/app/products/[id]/page.tsx
+++ src/app/products/[id]/page.tsx
@@ -1,9 +1,12 @@
 import prisma from '@/lib/db/prisma'
+import Link from 'next/link'
 
 import { Metadata } from 'next'
 import Image from 'next/image'
 import { notFound } from 'next/navigation'
 import { cache } from 'react'
+
+import ImageGalleryComp from '@/components/ImageGalleryComp'
 
 interface productsProps {
   params: {
@@ -37,20 +40,58 @@ export default async function singelProduactPage({
   const product = await getProdact(id)
 
   return (
-    <div className="flex flex-col gap-4 lg:flex-row lg:items-center">
-      <Image
-        src={product.imageUrl}
-        alt={product.name}
-        width={500}
-        height={500}
-      />
-      <div>
-        <h1 className="text-5xl font-bold">{product?.name}</h1>
-        <span className="badge mt-3 border-none bg-info px-3 text-white">
-          {product?.price}$
-        </span>
-        <p className="py-6">{product?.discription}</p>
+    <>
+      <div className="">
+        <ImageGalleryComp images={...product.images} />
+        <div className="flex flex-col items-center">
+          <h1 className="text-5xl font-bold">{product?.name}</h1>
+          <span className="badge mt-3 border-none bg-info px-3 text-white">
+            {product?.price}$
+          </span>
+          <p className="py-6">{product?.discription}</p>
+        </div>
       </div>
-    </div>
+    </>
   )
 }
+//<div className="flex  flex-row gap-4 lg:items-center">
+//!Carousel image slider
+/*<div className="carousel w-full">
+  <div id="item1" className="carousel-item w-full">
+    <img src="/images/stock/photo-1625726411847-8cbb60cc71e6.jpg" className="w-full" />
+  </div> 
+  <div id="item2" className="carousel-item w-full">
+    <img src="/images/stock/photo-1609621838510-5ad474b7d25d.jpg" className="w-full" />
+  </div> 
+  <div id="item3" className="carousel-item w-full">
+    <img src="/images/stock/photo-1414694762283-acccc27bca85.jpg" className="w-full" />
+  </div> 
+  <div id="item4" className="carousel-item w-full">
+    <img src="/images/stock/photo-1665553365602-b2fb8e5d1707.jpg" className="w-full" />
+  </div>
+</div> 
+<div className="flex justify-center w-full py-2 gap-2">
+  <a href="#item1" className="btn btn-xs">1</a> 
+  <a href="#item2" className="btn btn-xs">2</a> 
+  <a href="#item3" className="btn btn-xs">3</a> 
+  <a href="#item4" className="btn btn-xs">4</a>
+</div> */
+/*<div className="carousel w-full">
+        <div id="item" className="carousel-item w-full">
+          <Image
+            src={product.imageUrl}
+            alt={product.name}
+            width={900}
+            height={400}
+          />
+        </div>
+      </div>
+      <div className="flex max-h-2 w-full justify-center gap-2 py-2">
+        {product.images.map((img, index: number) => {
+          return (
+            <Link href={`#${index}`} key={index}>
+              <Image src={img} alt="product image" width={300} height={200} />
+            </Link>
+          )
+        })}
+      </div> */
---
+++ src/components/ImageGalleryComp.tsx
@@ -0,0 +1,27 @@
+'use client'
+import Carousel from 'react-material-ui-carousel'
+import Image from 'next/image'
+
+export default function ImageGalleryComp(images: any) {
+  return (
+    <Carousel>
+      {images.images.map((item: string, index: number) => (
+        <CarouselItem key={index} item={item} />
+      ))}
+    </Carousel>
+  )
+}
+const CarouselItem = ({ item }: any) => {
+  return (
+    <div className="flex flex-col items-center">
+      <Image
+        src={item}
+        alt={item.caption}
+        width={500}
+        height={400}
+        className="rounded-md"
+      />
+      <p>{item.caption}</p>
+    </div>
+  )
+}
