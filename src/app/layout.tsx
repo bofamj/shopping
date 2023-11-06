3397edd4e6336d5c29a417bc75c844967220d665
unknown <mustafamjaber@yahoo.com>
2023-11-06 14:07:56 +0200

    add the session provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import NavBar from './nav-bar/NavBar'
 import Footer from './footer'
+import SessionProvider from '@/app/SessionProvider '
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -19,12 +20,14 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <NavBar />
+        <SessionProvider>
+          <NavBar />
 
-        <main className=" mt-40 min-w-[300px] max-w-7xl  px-20 pt-10 md:mt-16">
-          {children}
-        </main>
-        <Footer />
+          <main className=" mt-40 min-w-[300px] max-w-7xl  px-20 pt-10 md:mt-16">
+            {children}
+          </main>
+          <Footer />
+        </SessionProvider>
       </body>
     </html>
   )
