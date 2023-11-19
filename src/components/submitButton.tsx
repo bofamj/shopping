b8917a559823194b3bc81db54d87061046aa752f
unknown <mustafamjaber@yahoo.com>
2023-11-19 12:54:19 +0200

    fix the submetbutton issue

--- src/components/submitButton.tsx
+++ src/components/submitButton.tsx
@@ -15,16 +15,10 @@ export default function SubmitButton({
   className,
   ...props
 }: SubmitButtonProbs) {
-  const { pending } = experimental_useFormState()
+  //const { pending } = experimental_useFormState()
 
   return (
-    <button
-      {...props}
-      disabled={pending}
-      type="submit"
-      className={`btn btn-primary ${className}`}
-    >
-      {pending && <span className="loading loading-dots loading-md" />}
+    <button {...props} type="submit" className={`btn btn-primary ${className}`}>
       {children}
     </button>
   )
